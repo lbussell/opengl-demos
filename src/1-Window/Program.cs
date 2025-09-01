a4e55e8cede1f251bcb7caccfedc91d65393046e
Logan Bussell <github@loganbussell.com>
2025-09-01 15:45:33 -0700

    Add 1-Window

---
+++ src/1-Window/Program.cs
@@ -0,0 +1,37 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2025 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+using Silk.NET.Input.Glfw;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Glfw;
+
+// This demo should display a plain black window.
+
+// Needed for Native AOT. Usually Silk.NET does this automatically with
+// reflection, but that doesn't work with Native AOT, so we have to do it
+// manually.
+GlfwWindowing.RegisterPlatform();
+GlfwInput.RegisterPlatform();
+
+var windowOptions = WindowOptions.Default;
+var window = Window.Create(windowOptions);
+
+window.Load += () =>
+{
+};
+
+window.Update += (double deltaTime) =>
+{
+};
+
+window.Render += (double deltaTime) =>
+{
+};
+
+window.FramebufferResize += (Vector2D<int> newSize) =>
+{
+};
+
+window.Run();
+window.Dispose();
